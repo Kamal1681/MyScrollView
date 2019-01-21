eae17657db97a62e8a3607641e66a690bd72d279
Kamal Maged <Kamal_Maged@yahoo.com>
2019-01-21 11:22:41 -0500

    Initial Commit

---
+++ MyScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyScrollView
+//
+//  Created by Kamal Maged on 2019-01-21.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
